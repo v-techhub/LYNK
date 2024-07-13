f99bd2a42ade2ada321bdbc77e6834b5b9add2fc
Victor Ezekiel <pianistpro8@gmail.com>
2024-07-13 16:17:04 +0100

    commit

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDlXCOy3Pbvp7DuM3pSv6o3MY8-OBUwb4M",
     authDomain: "lynk-4298b.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
     appId: "1:29511041935:web:185400f9651b1778f841fa"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom"
-import { PUBLIC_PATHS } from "./paths"
+import { PUBLIC_PATHS, PRIVATE_PATHS } from "./paths"
 import LoginRegisterTab from "@/screens/auth"
+import ChatBoard from "@/screens/private/ChatBoard"
 
 export const routes = createBrowserRouter([
     {
         path: PUBLIC_PATHS.AUTH,
         element: <LoginRegisterTab />
     },
-
+    {
+        path: PRIVATE_PATHS.CHATS_BOARD,
+        element: <ChatBoard />
+    }
 ])
